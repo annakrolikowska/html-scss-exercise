52f5bf76be93b26cb3d62a2494339f992fafdece
Anna <krolikowska.ann@gmail.com>
2023-03-05 21:41:24 +0100

    configuration of vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 /**
 * @type {import('vite').UserConfig}
 */
+import {defineConfing} from 'vite'
+
 export default {
     css: {
         devSourcemap: true,
     },
 }
+
+export default defineConfig({
+    base: '/html-scss-exercise/'
+})
